6f891370fe1b0e5989e4153bab3bed222fd56ed4
marylivingston21 <mliving1@uvm.edu>
2019-09-04 21:51:24 -0400

    error in step 44

--- pygameWindow.py
+++ pygameWindow.py
@@ -6,8 +6,9 @@ class PYGAME_WINDOW:
 		pygame.init()
 		self.screen = pygame.display.set_mode((300,100))
 
-	def Prepare():
-		pass
-
-	def Reveal():
+	def Prepare(self):
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				self.done = True
+	def Reveal(self):
 		pass
