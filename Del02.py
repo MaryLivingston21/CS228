f623f00ccd696b9a4a9f72a3cde3d805726fa2d7
marylivingston21 <mliving1@uvm.edu>
2019-09-09 22:25:05 -0400

    deliverable 2 up to step 13

--- Del02.py
+++ Del02.py
@@ -1,3 +1,4 @@
+import random
 import sys
 sys.path.insert(0,'..')
 from Leap import *
@@ -6,15 +7,16 @@ from Leap import *
 from pygameWindow import PYGAME_WINDOW
 from constants import CONSTANTS
 
-import random
-
 pygameWindow = PYGAME_WINDOW()
 myConstants = CONSTANTS()
+controller = Controller()
 
-xMin = 1000.0
-xMax = -1000.0
-yMin = 1000.0
-yMax = -1000.0
+x = int(constants.pygameWindowWidth / 2)
+y = int(constants.pygameWindowDepth / 2)
+xMin = 500.0
+xMax = -500.0
+yMin = 500.0
+yMax = -500.0
 
 #def Perturb_Circle_Position():
 	#global x, y
@@ -28,24 +30,64 @@ yMax = -1000.0
 	#elif fourSideDieRoll == 4:
 		##y += 1
 
-controller = Controller()
-
-#print pygameWindow
-
 def Handle_Frame(frame, xMin, xMax, yMin, yMax):
-	global x, y
+
 	hand = frame.hands[0]
 	fingers = hand.fingers
-	indexFingerList = fingers.finger_type(Finger.TYPE_INDEX)
-	indexFinger = indexFingerList[0]
 
-	distalPhalanx = indexFinger.bone(Bone.TYPE_DISTAL)
-	tip = distalPhalanx.next_joint
+	for finger in fingers:
+		Handle_Finger(finger)
+	#print finger
+	exit()
+	# indexFingerList = fingers.finger_type(Finger.TYPE_INDEX)
+	# indexFinger = indexFingerList[0]
+	#
+	# distalPhalanx = indexFinger.bone(Bone.TYPE_DISTAL)
+	# tip = distalPhalanx.next_joint
+	#
+	# x = tip[0]
+	# y = tip[1]
+	#
+	# #print(tip)
+	#
+	# if (x < xMin):
+	# 	xMin = x
+	# if (x > xMax):
+	# 	xMax = x
+	# if (y < yMin):
+	# 	yMin = y
+	# if y > yMax:
+	# 	yMax = y
 
-	x = tip[0]
-	y = tip[1]
+	#print xMin, xMax, yMin, yMax
 
-	#print(tip)
+def Scale(n, oMin, oMax, newMin, newMax):
+	if(oMin == oMax):
+		newN = n
+		return newN
+	old = oMax - oMin
+	new = newMax - newMin
+	newN = (n * new) / old
+	newN = newN + newMin
+	return newN
+
+def Handle_Finger(finger):
+	for b in range(0,4):
+		bone = finger.bone(b)
+
+		Handle_Bone(bone, 4 - b)
+
+def Handle_Bone(bone, width):
+	base = bone.prev_joint
+	tip = bone.next_joint
+	print base, tip
+
+def Handle_Vector_FromLeap(v):
+	global x, y
+	global xMin, xMax, yMin, yMax
+	x = Scale(v[0]), xMin, xMax, 0, myConstants.pygameWindowWidth)
+	y = Scale(v[2]), xMin, xMax, 0, myConstants.pygameWindowDepth)
+	print(x,y)
 
 	if (x < xMin):
 		xMin = x
@@ -56,18 +98,7 @@ def Handle_Frame(frame, xMin, xMax, yMin, yMax):
 	if y > yMax:
 		yMax = y
 
-	#print xMin, xMax, yMin, yMax
-
-def Scale(n,oMin,oMax,newMin,newMax):
-	newN = 0
-	if(oMin == oMax):
-		newN = n
-		return n
-	old = oMax - oMin
-	new = newMax - newMin
-	newN = (n * new) / old
-	newN = n + newMin
-	return n
+	return x, y
 
 while True:
 	pygameWindow.Prepare()
@@ -75,10 +106,9 @@ while True:
 	if len(frame.hands) != 0:
 		#print "hand detected"
 		Handle_Frame(frame, xMin, xMax, yMin, yMax)
-		pyGameX = Scale(x, xMin, xMax, 0, myConstants.pygameWindowWidth)
-		pyGamey = Scale(y, yMin, yMax, 0, myConstants.pygameWindowDepth)
-		pygameWindow.Draw_Black_Circle(int(pyGameX),int(pyGamey))
+		#pyGameX = Scale(x, xMin, xMax, 0, myConstants.pygameWindowWidth)
+		#pyGamey = Scale(y, yMin, yMax, 0, myConstants.pygameWindowDepth)
+		#pygameWindow.Draw_Black_Circle(int(pyGameX),int(pyGamey))
 		#print pyGameX, pyGamey
 	#Perturb_Circle_Position()
 	pygameWindow.Reveal()
-
